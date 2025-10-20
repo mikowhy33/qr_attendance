f88779a65c4c4de13f963033bbbae79067b8a827
mikowhy33 <koding252@gmail.com>
2025-10-20 13:09:15 +0200

    [feat]-set the timer, qr code disappears after some time

--- src/components/teacherPage.tsx
+++ src/components/teacherPage.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 
 type Data = {
@@ -11,29 +11,92 @@ export const TeacherPage = ({ src }: Data) => {
 
 	const [data, setData] = useState(src);
 
+	const [seconds, setSeconds] = useState<number>(5);
+
+	// we are keeping the reference to the interval!
+	const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+	// we control the visibility of the state
+	const [visible, setVisible] = useState(true);
+
 	const handleGenerateNew = async () => {
 		const res = await fetch("/api/qr", { cache: "no-cache" });
 		const data = await res.json();
 		setData(data);
 	};
+	// Wygenerowac funkcje, ktora odliczy 30 sekund, po tym czasie qr code ma zniknac!
+	// 1. Najpierw wygenerowac timer i wyswietlic funkcje
+
+	const timerForQrCode = () => {
+		if (intervalRef.current) {
+			clearInterval(intervalRef.current);
+		}
+
+		// every second the state will change and the seconds will go down
+		// we have to clean the interval
+
+		intervalRef.current = setInterval(() => {
+			setSeconds((prev) => {
+				// interval started
+				console.log("Tick:", prev);
+				if (prev <= 1) {
+					// interval ended
+					console.log("STOP interval");
+					clearInterval(intervalRef.current!);
+                    // we want this to
+					setVisible(false);
+					setData(src);
+					return 0;
+				}
+				return prev - 1;
+			});
+		}, 1000);
+	};
+
+	const handleClick = () => {
+		handleGenerateNew();
+		timerForQrCode();
+	};
+
+	// every time the app opens, it checks for running interval,
+	// if there are any it will be cleared!
+	useEffect(() => {
+		return () => {
+			if (intervalRef.current) {
+				clearInterval(intervalRef.current);
+			}
+		};
+	}, []);
 
 	return (
 		<>
-        {/* We are taking whole space from the body, also width is a screen */}
+			{/* We are taking whole space from the body, also width is a screen */}
 			<div className="flex flex-col flex-1 items-center w-screen  gap-3 p-5">
-				<Button onClick={handleGenerateNew} className="max-w-2xs">
+				<Button onClick={handleClick} className="max-w-2xs">
 					{" "}
 					Click to generate a QR code{" "}
 				</Button>
+				<p>Seconds untill the QR code dissapears! {seconds}</p>
+
 				{src || data ? (
-                    // we are taking the rest of free space and center it
-					<div className="flex flex-1 w-full justify-center items-center">
-						{/* gotta access obj qr code */}
-                        {/* object-contain, scales the img, it will not be blurry, in smaller/ bigger screens the qr code always will look nice! */}
-						<img src={data.qr} className="w-128 h-128  object-contain " ></img>
+                    // position relative so the divs position will match this
+					<div className="flex flex-1 w-full justify-center items-center relative">
+						<div
+						className={`transition-opacity duration-1000 ${
+							visible ? "opacity-100" : "opacity-0"
+						}`}>
+						
+							<img src={data.qr} className="w-128 h-128 object-contain" />
+						</div>
+						{visible===false && (
+                            // position absolute, not in the normal flow, getting him out
+							<div className="absolute text-red-500 text-lg">
+								Your time has ended!
+							</div>
+						)}
 					</div>
 				) : (
-					<p>Generating..</p>
+					<p>Generating...</p>
 				)}
 			</div>
 		</>
